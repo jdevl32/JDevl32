452d6609b2fc974f75a3cd2a501db701fbe4d76d
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-17 17:39:55 -0500

    Modify batch script for initialization of first package.

--- .nuget/init/run.bat
+++ .nuget/init/run.bat
@@ -1,9 +1,10 @@
 @echo off
 
-set "src=..\.."
+set "src=..\..\JDevl32.Web\bin\Release"
 set "dst=\\dd-wrt\nas-t-a1\[Storage]\Development\Package\NuGet"
+set "dstls=//dd-wrt/nas-t-a1/[Storage]/Development/Package/NuGet"
 
 @rem @echo %cd%
 
 nuget init "%src%" "%dst%"
-ls -lA "%dst%"
+ls -lA "%dstls%"

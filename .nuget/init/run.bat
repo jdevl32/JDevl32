afd9cb8e67ab29201ea32ebd76e21b914b6687fd
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-17 23:28:20 -0500

    Enhance package source path (as post build command).

--- .nuget/init/run.bat
+++ .nuget/init/run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "src=..\..\JDevl32.Web\bin\Release"
+set "src=C:\Offline\[D Drive]\[Storage]\Development\Package\NuGet\Release"
 set "dst=\\dd-wrt\nas-t-a1\[Storage]\Development\Package\NuGet"
 @rem todo|jdevl32: use "dst" to replace slashes
 set "dstls=//dd-wrt/nas-t-a1/[Storage]/Development/Package/NuGet"

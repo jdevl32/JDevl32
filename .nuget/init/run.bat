d6d9473fc546cbe4c46d217e58fd96cb69613272
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-17 23:14:54 -0500

    Add todo comment.

--- .nuget/init/run.bat
+++ .nuget/init/run.bat
@@ -2,6 +2,7 @@
 
 set "src=..\..\JDevl32.Web\bin\Release"
 set "dst=\\dd-wrt\nas-t-a1\[Storage]\Development\Package\NuGet"
+@rem todo|jdevl32: use "dst" to replace slashes
 set "dstls=//dd-wrt/nas-t-a1/[Storage]/Development/Package/NuGet"
 
 @rem @echo %cd%

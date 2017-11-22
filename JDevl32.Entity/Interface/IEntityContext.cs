6b7bf5cef72aa1b2655e193d0fa943c640cc9f77
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-21 22:11:06 -0500

    Add entity context.

---
+++ JDevl32.Entity/Interface/IEntityContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace JDevl32.Entity.Interface
+{
+
+	/// <summary>
+	/// The entity context.
+	/// </summary>
+	/// <remarks>
+	/// Last modification:
+	/// </remarks>
+	public interface IEntityContext
+	{
+
+#region Property
+
+		/// <summary>
+		/// The configuration root of the application.
+		/// </summary>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		IConfigurationRoot ConfigurationRoot { get; }
+
+		/// <summary>
+		/// The hosting environment of the application.
+		/// </summary>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		IHostingEnvironment HostingEnvironment { get; }
+
+#endregion
+
+	}
+
+}

9d5b7f91539a4871740c64e39d5fe0cd9ec8a1d0
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-21 20:19:56 -0500

    Implement global and unique base.

---
+++ JDevl32.Entity/Model/GlobalUniqueBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace JDevl32.Entity.Model
+{
+
+	/// <inheritdoc />
+	/// <summary>
+	/// Global unique (entity) item.
+	/// </summary>
+	/// <remarks>
+	/// Last modification:
+	/// </remarks>
+	public abstract class GlobalUniqueBase
+		:
+		UniqueBase
+	{
+
+#region Overrides of UniqueBase
+
+		/// <summary>
+		/// The global unique identifier.
+		/// </summary>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		public new Guid Id { get; }
+
+#endregion
+
+#region Instance Initialization
+
+		/// <inheritdoc />
+		/// <summary>
+		/// Create a global unique (entity) item.
+		/// </summary>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected GlobalUniqueBase()
+			:
+			this(Guid.NewGuid())
+		{
+		}
+
+		/// <inheritdoc />
+		protected GlobalUniqueBase(Guid id)
+			:
+			base(id.GetHashCode()) => Id = id;
+
+		/// <inheritdoc />
+		protected GlobalUniqueBase(Guid id, string shortName, string fullName, string description)
+			:
+			base(id.GetHashCode(), shortName, fullName, description) => Id = id;
+
+#endregion
+
+	}
+
+}
---
+++ JDevl32.Entity/Model/UniqueBase.cs
@@ -0,0 +1,98 @@
+﻿using JDevl32.Entity.Interface;
+using System.ComponentModel.DataAnnotations;
+
+namespace JDevl32.Entity.Model
+{
+
+	/// <inheritdoc />
+	/// <summary>
+	/// Unique (entity) item.
+	/// </summary>
+	/// <remarks>
+	/// Last modification:
+	/// </remarks>
+	public abstract class UniqueBase
+		:
+		IUnique
+	{
+
+#region IUnique
+
+		/// <inheritdoc />
+		[Key]
+		public virtual int Id { get; }
+
+		/// <inheritdoc />
+		public string ShortName { get; }
+
+		/// <inheritdoc />
+		public string FullName { get; }
+
+		/// <inheritdoc />
+		public string Description { get; }
+
+#endregion
+
+#region Instance Initialization
+
+		/// <summary>
+		/// Create a unique item.
+		/// </summary>
+		/// <param name="id">
+		/// The unique identifier.
+		/// </param>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected UniqueBase(int id) => Id = id;
+
+		/// <inheritdoc />
+		/// <summary>
+		/// Create a unique item.
+		/// </summary>
+		/// <param name="id">
+		/// The unique identifier.
+		/// </param>
+		/// <param name="shortName">
+		/// The short name.
+		/// </param>
+		/// <param name="fullName">
+		/// The full (long) name.
+		/// </param>
+		/// <param name="description">
+		/// The description.
+		/// </param>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected UniqueBase(int id, string shortName, string fullName, string description)
+			:
+			this(shortName, fullName, description) => Id = id;
+
+		/// <summary>
+		/// Create a unique item.
+		/// </summary>
+		/// <param name="shortName">
+		/// The short name.
+		/// </param>
+		/// <param name="fullName">
+		/// The full (long) name.
+		/// </param>
+		/// <param name="description">
+		/// The description.
+		/// </param>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected UniqueBase(string shortName, string fullName, string description)
+		{
+			ShortName = shortName;
+			FullName = fullName;
+			Description = description;
+		}
+
+#endregion
+
+	}
+	
+}

88e759f6983a1c97e9e841429bf6d08ee693c5f5
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-21 22:51:01 -0500

    Add entity context base.

---
+++ JDevl32.Entity/Model/EntityContextBase.cs
@@ -0,0 +1,57 @@
+﻿using JDevl32.Entity.Interface;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace JDevl32.Entity.Model
+{
+
+	/// <summary>
+	/// The entity context base class.
+	/// </summary>
+	/// <remarks>
+	/// Last modification:
+	/// </remarks>
+	public abstract class EntityContextBase
+		:
+		DbContext
+		,
+		IEntityContext
+	{
+
+#region IEntityContext
+
+		/// <inheritdoc />
+		public IConfigurationRoot ConfigurationRoot { get; }
+
+		/// <inheritdoc />
+		public IHostingEnvironment HostingEnvironment { get; }
+
+#endregion
+
+#region Instance Initialization
+
+		/// <inheritdoc />
+		/// <summary>
+		/// Create an entity context base.
+		/// </summary>
+		/// <param name="configurationRoot">
+		/// The configuration root of the application.
+		/// </param>
+		/// <param name="hostingEnvironment">
+		/// The hosting envrionment of the application.
+		/// </param>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected EntityContextBase(IConfigurationRoot configurationRoot, IHostingEnvironment hostingEnvironment)
+		{
+			ConfigurationRoot = configurationRoot;
+			HostingEnvironment = hostingEnvironment;
+		}
+
+#endregion
+
+	}
+
+}

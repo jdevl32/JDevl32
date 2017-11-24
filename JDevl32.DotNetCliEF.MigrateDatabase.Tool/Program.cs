8523adaa4236211e732cbf61a945e3196e163bd9
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-11-24 01:32:02 -0500

    Add database migration tool.

---
+++ JDevl32.DotNetCliEF.MigrateDatabase.Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JDevl32.DotNetCliEF.MigrateDatabase.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

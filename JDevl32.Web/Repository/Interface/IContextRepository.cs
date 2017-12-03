8dfc6af3ddab2ebd414a1e111c71ab6499d353a9
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-12-03 00:40:59 -0500

    Add context repository interface.

---
+++ JDevl32.Web/Repository/Interface/IContextRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace JDevl32.Web.Repository.Interface
+{
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <remarks>
+	/// Last modification:
+	/// </remarks>
+	public interface IContextRepository
+	{
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>
+		/// 
+		/// </returns>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		Task<bool> SaveChangesAsync();
+
+	}
+
+}

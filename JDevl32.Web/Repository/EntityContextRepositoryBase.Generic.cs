a314e2ca236decdd4404c219292d709dddcab9fc
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-12-06 21:39:19 -0500

    Add entity context type parameter. Implement instance mapper. Add constructor.

--- JDevl32.Web/Repository/EntityContextRepositoryBase.Generic.cs
+++ JDevl32.Web/Repository/EntityContextRepositoryBase.Generic.cs
@@ -1,5 +1,7 @@
-﻿using JDevl32.Entity.Interface;
+﻿using AutoMapper;
+using JDevl32.Entity.Interface;
 using JDevl32.Logging.Interface;
+using JDevl32.Mapper.Interface;
 using JDevl32.Web.Repository.Interface;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
@@ -12,17 +14,25 @@ namespace JDevl32.Web.Repository
 	/// </summary>
 	/// <remarks>
 	/// Last modification:
-	/// Re-implement entity context respository as non-generic.
+	/// Add entity context type parameter.
+	/// Implement instance mapper.
+	/// Add constructor.
 	/// </remarks>
-	public abstract class EntityContextRepositoryBase<TDerivedClass>
+	public abstract class EntityContextRepositoryBase<TDerivedClass, TEntityContext>
 		:
 		IEntityContextRepository
 		,
 		ILoggable<TDerivedClass>
+		,
+		IInstanceMapper
 		where
 			TDerivedClass
 			:
 			class
+		where
+			TEntityContext
+			:
+			IEntityContext
 	{
 
 #region Property
@@ -30,22 +40,80 @@ namespace JDevl32.Web.Repository
 #region IEntityContextRepository
 
 		/// <inheritdoc />
-		public abstract IEntityContext EntityContext { get; }
-
-#endregion
+		/// <remarks>
+		/// Last modification:
+		/// Re-implement explicitly.
+		/// </remarks>
+		IEntityContext IEntityContextRepository.EntityContext
+		{
+			get => Mapper.Map<IEntityContext>(EntityContext);
+			// todo|jdevl32: is the setter needed ???
+		}
 
 #endregion
 
 #region ILoggable<TDerivedClass>
 
 		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
 		public virtual ILogger<TDerivedClass> Logger { get; }
+
+#endregion
+
+#region IInstanceMapper
+
+		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		public IMapper Mapper { get; }
+
+#endregion
+
+		/// <summary>
+		/// The entity context.
+		/// </summary>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		public virtual TEntityContext EntityContext { get; }
+
+#endregion
+
+#region Instance Initialization
+
+		/// <summary>
+		/// Create an entity context repository.
+		/// </summary>
+		/// <param name="entityContext">
+		/// The entity context.
+		/// </param>
+		/// <param name="logger">
+		/// The logger.
+		/// </param>
+		/// <param name="mapper">
+		/// The mapper.
+		/// </param>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected EntityContextRepositoryBase(TEntityContext entityContext, ILogger<TDerivedClass> logger, IMapper mapper)
+		{
+			EntityContext = entityContext;
+			Logger = logger;
+			Mapper = mapper;
+		}
 
 #endregion
 
 #region IEntityContextRepository
 
 		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
 		public virtual async Task<bool> SaveChangesAsync() => await EntityContext.SaveChangesAsync() > 0;
 
 #endregion

cf2a5429310459192ec78024b22ffbddabc93492
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2017-12-08 01:43:17 -0500

    Add default (parameterless) consctructor (required by Entity Framework).

--- JDevl32.Entity/Model/UniqueBase.cs
+++ JDevl32.Entity/Model/UniqueBase.cs
@@ -7,7 +7,7 @@ namespace JDevl32.Entity.Model
 	/// <inheritdoc />
 	/// <remarks>
 	/// Last modification:
-	/// Add setters (required by Entity Framework).
+	/// Add default (parameterless) consctructor (required by Entity Framework).
 	/// </remarks>
 	public abstract class UniqueBase
 		:
@@ -17,22 +17,44 @@ namespace JDevl32.Entity.Model
 #region IUnique
 
 		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
 		[Key]
 		[Required]
 		public virtual int Id { get; set; }
 
 		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
 		public string ShortName { get; set; }
 
 		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
 		public string FullName { get; set; }
 
 		/// <inheritdoc />
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
 		public string Description { get; set; }
 
 #endregion
 
 #region Instance Initialization
+
+		/// <summary>
+		/// Create a unique item.
+		/// </summary>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		protected UniqueBase()
+		{
+		}
 
 		/// <summary>
 		/// Create a unique item.

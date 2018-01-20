f38bcc9a8d123d637febfc3ce962f4ec341b45a7
Jeremy Paiz | jdevl32 <2f0a8178@opayq.com>
2018-01-19 19:56:54 -0500

    Add extension to the unique item interface.

---
+++ JDevl32.Entity/Common.cs
@@ -0,0 +1,31 @@
+﻿using JDevl32.Entity.Interface;
+
+namespace JDevl32.Entity
+{
+
+	/// <summary>
+	/// A common (utility) class.
+	/// </summary>
+	/// <remarks>
+	/// Last modification:
+	/// </remarks>
+	public static class Common
+	{
+
+		/// <summary>
+		/// Get a string representation of a unique item.
+		/// </summary>
+		/// <param name="uniqueItem">
+		/// The unique item.
+		/// </param>
+		/// <returns>
+		/// A string representation of the unique item.
+		/// </returns>
+		/// <remarks>
+		/// Last modification:
+		/// </remarks>
+		public static string ToString(IUnique uniqueItem) => $"[{nameof(uniqueItem.Id)}={uniqueItem.Id}|{nameof(uniqueItem.ShortName)}={uniqueItem.ShortName}|{nameof(uniqueItem.FullName)}={uniqueItem.FullName}|{nameof(uniqueItem.Description)}={uniqueItem.Description}]";
+
+	}
+
+}
